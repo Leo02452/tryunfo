dbf907f9009b5eb1c873bc9ca04384bb35bec12d
Leo Araujo <leonardo_02452@hotmail.com>
2022-02-23 20:18:21 -0300

    Fix submit button e adicionada as props a receber (req 2)

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,20 +1,29 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Form extends Component {
   render() {
+    const {
+      cardName, cardDescription, cardAttr1, cardAttr2,
+      cardAttr3, cardImage, cardRare, cardTrunfo,
+      hasTrunfo, isSaveButtonDisabled, onInputChange, onSaveButtonClick } = this.props;
     return (
-      <Form>
+      <form>
         <label htmlFor="name-input">
           <input
             type="text"
             data-testid="name-input"
             name="name-input"
+            value={ cardName }
+            onChange={ onInputChange }
           />
         </label>
         <label htmlFor="description-input">
           <textarea
             data-testid="description-input"
             name="description-input"
+            value={ cardDescription }
+            onChange={ onInputChange }
           />
         </label>
         <label htmlFor="attr1-input">
@@ -22,6 +31,8 @@ class Form extends Component {
             type="number"
             data-testid="attr1-input"
             name="attr1-input"
+            value={ cardAttr1 }
+            onChange={ onInputChange }
           />
         </label>
         <label htmlFor="attr2-input">
@@ -29,6 +40,8 @@ class Form extends Component {
             type="number"
             data-testid="attr2-input"
             name="attr2-input"
+            value={ cardAttr2 }
+            onChange={ onInputChange }
           />
         </label>
         <label htmlFor="attr3-input">
@@ -36,6 +49,8 @@ class Form extends Component {
             type="number"
             data-testid="attr3-input"
             name="attr3-input"
+            value={ cardAttr3 }
+            onChange={ onInputChange }
           />
         </label>
         <label htmlFor="image-input">
@@ -43,9 +58,15 @@ class Form extends Component {
             type="text"
             data-testid="image-input"
             name="image-input"
+            value={ cardImage }
+            onChange={ onInputChange }
           />
         </label>
-        <select data-testid="rare-input">
+        <select
+          data-testid="rare-input"
+          value={ cardRare }
+          onChange={ onInputChange }
+        >
           <option>normal</option>
           <option>raro</option>
           <option>muito raro</option>
@@ -55,12 +76,36 @@ class Form extends Component {
             type="checkbox"
             data-testid="trunfo-input"
             name="trunfo-input"
+            checked={ cardTrunfo }
+            onChange={ onInputChange }
           />
         </label>
-        <button type="button" data-testid="save-button">Salvar</button>
-      </Form>
+        <button
+          type="submit"
+          data-testid="save-button"
+          disabled={ isSaveButtonDisabled }
+          onClick={ onSaveButtonClick }
+        >
+          Salvar
+        </button>
+      </form>
     );
   }
 }
+
+Form.propTypes = {
+  cardName: PropTypes.string.isRequired,
+  cardDescription: PropTypes.string.isRequired,
+  cardAttr1: PropTypes.string.isRequired,
+  cardAttr2: PropTypes.string.isRequired,
+  cardAttr3: PropTypes.string.isRequired,
+  cardImage: PropTypes.string.isRequired,
+  cardRare: PropTypes.string.isRequired,
+  cardTrunfo: PropTypes.bool.isRequired,
+  hasTrunfo: PropTypes.bool.isRequired,
+  isSaveButtonDisabled: PropTypes.bool.isRequired,
+  onInputChange: PropTypes.func.isRequired,
+  onSaveButtonClick: PropTypes.func.isRequired,
+};
 
 export default Form;
